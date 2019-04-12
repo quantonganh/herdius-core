4709cff0b98e2bed1d4132ce7695a0de4eb4b7dd
Cole Bittel <cole.bittel@gmail.com>
2019-04-12 15:43:59 +0200

    Set gomodules on

--- deployment/kill_old_server.sh
+++ deployment/kill_old_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-echo "attempting to kill old server process"
+echo "attempting to kill old server proc"
 pkill -f /tmp/go-build
 echo "old server killed"
--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -2,11 +2,12 @@
 set -ex
 
 export PATH=$PATH:/usr/local/go/bin
+export GO111MODULE=on
 export GOPATH=/home/ec2-user/go
 whoami
 ls -lah
 pwd
 cd /home/ec2-user/go/src/github.com/herdius/herdius-core
 go get ./...
-make start-server ENV=staging > /dev/null 2> /dev/null < /dev/null &
+make start-supervisor ENV=staging > /dev/null 2> /dev/null < /dev/null &
 echo "server started in background"
