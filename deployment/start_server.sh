bc083777c170cdda4330f955dfaa59e77f78908f
Cole Bittel <cole.bittel@gmail.com>
2019-05-06 15:39:02 +0200

    variabalize

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+LOGDIR=/var/log/herdius/herdius-core/log
 export PATH=$PATH:/usr/local/go/bin
 export GO111MODULE=on
 export GOPATH=/home/ec2-user/go
 cd /home/ec2-user/go/src/github.com/herdius/herdius-core
-mkdir -p /var/log/herdius/herdius-core/log/
+sudo mkdir -p $LOGDIR
 sudo chmod 733 -R /var/log/herdius/
 go get ./...
-make start-supervisor ENV=staging > /var/log/herdius/herdius-core/log/server.log 2> /var/log/herdius/herdius-core/log/server.log < /dev/null &
+make start-supervisor ENV=staging > $LOGDIR/server.log 2> $LOGDIR/server.log < /dev/null &
 echo "server started in background"
