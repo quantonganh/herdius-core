87d69c748aeb2116c915f98d72e7744fb0d8af7b
Cole Bittel <cole.bittel@gmail.com>
2019-05-06 14:47:22 +0200

    Log to file and rewrite; not stdout

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -4,10 +4,9 @@ set -ex
 export PATH=$PATH:/usr/local/go/bin
 export GO111MODULE=on
 export GOPATH=/home/ec2-user/go
-whoami
-ls -lah
-pwd
 cd /home/ec2-user/go/src/github.com/herdius/herdius-core
+mkdir -p /var/log/herdius/herdius-core/log/
+sudo chmod 733 -R /var/log/herdius/
 go get ./...
-make start-supervisor ENV=staging > /dev/null 2> /dev/null < /dev/null &
+make start-supervisor ENV=staging > /var/log/herdius/herdius-core/log/server.log 2> /var/log/herdius/herdius-core/log/server.log < /dev/null &
 echo "server started in background"
