a33afdf98bb080bc6dbf85c8f2a9b52fa962191a
Cole Bittel <cole.bittel@gmail.com>
2019-05-24 08:23:08 +0200

    Now uploading file with correct size and all

--- aws/aws.go
+++ aws/aws.go
@@ -17,7 +17,7 @@ import (
 )
 
 // BackupBaseBlock takes a single block
-func BackupBaseBlock(env string, lastblock, baseBlock *protobuf.BaseBlock) error {
+func BackupBaseBlock(env string, lastblock, baseBlock *protobuf.BaseBlock) (err error) {
 	sess := session.Must(session.NewSession())
 
 	bBlock, err := json.Marshal(baseBlock)
@@ -25,13 +25,35 @@ func BackupBaseBlock(env string, lastblock, baseBlock *protobuf.BaseBlock) error
 		return fmt.Errorf("cannot convert baseBlock to json: %v", err)
 	}
 	fileName := "tmpfile.txt"
-	err = ioutil.WriteFile(fileName, bBlock, 0644)
+
+	//file := ioutil.WriteFile(filenam), bBlock, 0644)
+	tmpFile, err := ioutil.TempFile("/Users/bittelc/go/src/github.com/herdius/herdius-blockchain-api", fileName)
 	if err != nil {
-		return fmt.Errorf("cannot write to %v: %v", fileName, err)
+		return fmt.Errorf("cannot create tmpfile %v: %v", fileName, err)
 	}
-	file, err := os.Open(fileName)
+	defer os.Remove(tmpFile.Name())
+
+	/// WHY ARENT YOU WRITING
+	n, err := tmpFile.Write(bBlock)
+	/// WHY ARENT YOU WRITING
+
 	if err != nil {
-		return fmt.Errorf("cannot read from %v: %v", fileName, err)
+		return fmt.Errorf("cannot write to: tmpfile %v: %v", fileName, err)
+	}
+	defer func() {
+		err = tmpFile.Close()
+		if err != nil {
+			err = fmt.Errorf("cannot close tmpfile %v: %v", fileName, err)
+		}
+	}()
+	log.Printf("tmpfile wrote %v byte", n)
+	err = tmpFile.Sync()
+	if err != nil {
+		return fmt.Errorf("cannot sync file %v: %v", fileName, err)
+	}
+	_, err = tmpFile.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("cannot seek file %v: %v", fileName, err)
 	}
 
 	bucket := config.GetConfiguration(env).S3Bucket
@@ -42,7 +64,7 @@ func BackupBaseBlock(env string, lastblock, baseBlock *protobuf.BaseBlock) error
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket:               aws.String(bucket),
 		Key:                  aws.String(fmt.Sprintf("%v-%v", heightStr, timeStamp)),
-		Body:                 file,
+		Body:                 tmpFile,
 		ServerSideEncryption: aws.String("AES256"),
 		Tagging:              aws.String(fmt.Sprintf("height=%v&timestamp=%v", heightStr, timeStamp)),
 	})
@@ -50,5 +72,5 @@ func BackupBaseBlock(env string, lastblock, baseBlock *protobuf.BaseBlock) error
 		return fmt.Errorf("failed to upload file, %v", err)
 	}
 	log.Println("Uploaded file to S3:", result)
-	return nil
+	return
 }
