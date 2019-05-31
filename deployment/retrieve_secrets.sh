acf1667a529f94ca46c25f24dc59bc3cb352bdcf
Cole Bittel <cole.bittel@gmail.com>
2019-05-31 16:46:15 +0200

    test chmod method

--- deployment/retrieve_secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-export INFURAID=$(aws secretsmanager get-secret-value --secret-id INFURAID --query SecretString --region=us-east-1 --output text | jq -r .INFURAID)
-echo "INFURAID VALUE IS:"
-echo "PWD: $PWD"
-echo "$INFURAID" >> /home/ec2-user/go/src/github.com/herdius/herdius-core/env_vars.log
--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -12,9 +12,6 @@ sudo chmod 733 -R /var/log/herdius/
 go get ./...
 
 export INFURAID=$(aws secretsmanager get-secret-value --secret-id INFURAID --query SecretString --region=us-east-1 --output text | jq -r .INFURAID)
-echo "INFURAID VALUE IS:"
-echo "PWD: $PWD"
-echo "$INFURAID" >> /home/ec2-user/go/src/github.com/herdius/herdius-core/env_vars.log
 
 make start-supervisor ENV=staging > $LOGDIR/server.log 2> $LOGDIR/server.log < /dev/null &
 echo "server started in background"
