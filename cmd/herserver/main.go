939409a55861493cd9d587a9e4310c69fe4ff620
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-08-21 16:20:56 +0700

    cmd/herserver: remove validator immediately when disconnect
    Removing validator when disconnect is done synchronously after a block
    time. We want to it to be removed immediately when disconnection occurs,
    so moving the check in separated goroutine. It is actually have a 1
    second delay, it is good enough, we can increase or remove it later if
    it affects.
    
    While at it, also refactoring the code.

--- cmd/herserver/main.go
+++ cmd/herserver/main.go
@@ -33,29 +33,21 @@ import (
 	amino "github.com/tendermint/go-amino"
 )
 
-var cdc = amino.NewCodec()
-var supsvc *sup.Supervisor
-var blockchainSvc *blockchain.Service
-var voteCount = 0
+var (
+	cdc        = amino.NewCodec()
+	nodeKeydir = "./cmd/testdata/secp205k1Accts/"
+)
 
-// Flag to check if a new child block has arrived to validator
-var isChildBlockReceivedByValidator = false
-
-// Child block message object received
-var mcb = &blockProtobuf.ChildBlockMessage{}
-
-var nodeKeydir = "./cmd/testdata/secp205k1Accts/"
-var t1 time.Time
-var t2 time.Time
-var addresses = make([]string, 0)
-
-var accountStorage external.BalanceStorage
+var (
+	supsvc         *sup.Supervisor
+	blockchainSvc  *blockchain.Service
+	accountStorage external.BalanceStorage
+)
 
 // HerdiusMessagePlugin will receive all transmitted messages.
 type HerdiusMessagePlugin struct{ *network.Plugin }
 
 func init() {
-
 	nlog.SetFlags(nlog.LstdFlags | nlog.Lshortfile)
 	supsvc = &sup.Supervisor{}
 	supsvc.SetWriteMutex()
@@ -63,13 +55,8 @@ func init() {
 	supsvc.ChildBlock = make([]*blockProtobuf.ChildBlock, 0)
 	supsvc.VoteInfoData = make(map[string][]*blockProtobuf.VoteInfo, 0)
 
-	RegisterAminoService(cdc)
-}
-
-//RegisterAminoService registers Amino service for message (en/de) coding
-func RegisterAminoService(cdc *amino.Codec) {
+	// Register Amino service for message (en/de) coding
 	cryptoAmino.RegisterAmino(cdc)
-
 }
 
 // Receive handles each received message for both Supervisor and Validator
@@ -78,28 +65,29 @@ func (state *HerdiusMessagePlugin) Receive(ctx *network.PluginContext) error {
 	case *blockProtobuf.ConnectionMessage:
 		address := ctx.Client().ID.Address
 		pubKey := ctx.Client().ID.PublicKey
-		err := supsvc.AddValidator(pubKey, address)
-		if err != nil {
-			log.Info().Msgf("<%s> Failed to add validator: %v", address, err)
+		if err := supsvc.AddValidator(pubKey, address); err != nil {
+			log.Error().Err(err).Msgf("<%s> Failed to add validator", address)
+			return err
 		}
 
-		addresses = append(addresses, address)
 		log.Info().Msgf("<%s> %s", address, msg.Message)
 
 		// This map will be used to map validators to their respective child blocks
-		mx := supsvc.GetMutex()
-		mx.Lock()
+		mu := supsvc.GetMutex()
+		mu.Lock()
 		supsvc.ValidatorChildblock[address] = &blockProtobuf.BlockID{}
-		mx.Unlock()
+		mu.Unlock()
 
 		sender, err := ctx.Network().Client(ctx.Client().Address)
 		if err != nil {
 			return fmt.Errorf("failed to get client network: %v", err)
 		}
 		nonce := 1
-		err = sender.Reply(network.WithSignMessage(context.Background(), true), uint64(nonce),
-			&blockProtobuf.ConnectionMessage{Message: "Connection established with Supervisor"})
-		if err != nil {
+		if err := sender.Reply(
+			network.WithSignMessage(context.Background(), true),
+			uint64(nonce),
+			&blockProtobuf.ConnectionMessage{Message: "Connection established with Supervisor"},
+		); err != nil {
 			return fmt.Errorf(fmt.Sprintf("Failed to reply to client: %v", err))
 		}
 	}
@@ -107,16 +95,15 @@ func (state *HerdiusMessagePlugin) Receive(ctx *network.PluginContext) error {
 }
 
 func main() {
-
 	// process other flags
 	peersFlag := flag.String("peers", "", "peers to connect to")
-	supervisorFlag := flag.Bool("supervisor", false, "run as supervisor")
 	groupSizeFlag := flag.Int("groupsize", 3, "# of peers in a validator group")
 	portFlag := flag.Int("port", 0, "port to bind validator to")
 	envFlag := flag.String("env", "dev", "environment to build network and run process for")
 	waitTimeFlag := flag.Int("waitTime", 15, "time to wait before the Memory Pool is flushed to a new block")
 	restoreFlag := flag.Bool("restore", false, "restore blockchain from S3")
 	backupFlag := flag.Bool("backup", false, "backup blockchain to S3")
+
 	flag.Parse()
 
 	noOfPeersInGroup := *groupSizeFlag
@@ -125,18 +112,18 @@ func main() {
 	waitTime := *waitTimeFlag
 	restr := *restoreFlag
 	backup := *backupFlag
-	confg := config.GetConfiguration(env)
+	cfg := config.GetConfiguration(env)
 	peers := []string{}
 	if len(*peersFlag) > 0 {
 		peers = strings.Split(*peersFlag, ",")
 	}
 
 	if port == 0 {
-		port = confg.SelfBroadcastPort
+		port = cfg.SelfBroadcastPort
 	}
 
 	// Generate or Load Keys
-	nodeAddress := confg.SelfBroadcastIP + "_" + strconv.Itoa(port)
+	nodeAddress := cfg.SelfBroadcastIP + "_" + strconv.Itoa(port)
 	nodekey, err := keystore.LoadOrGenNodeKey(nodeKeydir + nodeAddress + "_sk_peer_id.json")
 	if err != nil {
 		log.Error().Msgf("Failed to create or load node key: %v", err)
@@ -175,12 +162,11 @@ func main() {
 	opcode.RegisterMessageType(types.OpcodeTxsByBlockHeightRequest, &protoplugin.TxsByBlockHeightRequest{})
 	opcode.RegisterMessageType(types.OpcodeLastBlockRequest, &protoplugin.LastBlockRequest{})
 
-	config := config.GetConfiguration(env)
-	address := config.ConstructTCPAddress()
+	address := cfg.ConstructTCPAddress()
 	builder := network.NewBuilderWithOptions(network.Address(address))
 	builder.SetKeys(keys)
 
-	builder.SetAddress(network.FormatAddress(confg.Protocol, confg.SelfBroadcastIP, uint16(port)))
+	builder.SetAddress(network.FormatAddress(cfg.Protocol, cfg.SelfBroadcastIP, uint16(port)))
 
 	// Register peer discovery plugin.
 	builder.AddPlugin(new(discovery.Plugin))
@@ -193,7 +179,7 @@ func main() {
 
 	net, err := builder.Build()
 	if err != nil {
-		log.Fatal().Err(err)
+		log.Fatal().Err(err).Msg("failed to build network")
 		return
 	}
 
@@ -209,93 +195,91 @@ func main() {
 	// Chain data and state information will be stored at supervisor's node.
 
 	var stateRoot []byte
-	if *supervisorFlag {
-		accountStorage = external.New()
-		if restr {
-			nlog.Println("Restore value true: proceeding to restore from AWS S3")
-			r := restore.NewRestorer(env, 3)
-			err := r.Restore()
-			if err != nil {
-				log.Error().Msg(err.Error())
-			}
-		}
-		blockchain.LoadDB()
-		sup.LoadStateDB(accountStorage)
-		blockchainSvc := &blockchain.Service{}
-
-		lastBlock := blockchainSvc.GetLastBlock()
-
-		go syncer.SyncAllAccounts(accountStorage)
-
-		if err != nil {
-			log.Error().Msgf("Failed while getting last block: %v\n", err)
-		} else {
-			var lbh cmn.HexBytes
-			lastBlockHash := lastBlock.GetHeader().GetBlock_ID().GetBlockHash()
-			lbh = lastBlockHash
-			lbHeight := lastBlock.GetHeader().GetHeight()
-
-			log.Info().Msgf("Last Block Hash: %v", lbh)
-			log.Info().Msgf("Height: %v", lbHeight)
-
-			s := lastBlock.GetHeader().GetTime().GetSeconds()
-			ts := time.Unix(s, 0)
-			log.Info().Msgf("Timestamp: %v", ts)
-
-			var stateRootHex cmn.HexBytes
-			stateRoot = lastBlock.GetHeader().GetStateRoot()
-			stateRootHex = stateRoot
-			log.Info().Msgf("State root: %v", stateRootHex)
-
+	accountStorage = external.New()
+	if restr {
+		log.Info().Msg("Restore value true: proceeding to restore from AWS S3")
+		r := restore.NewRestorer(env, 3)
+		if err := r.Restore(); err != nil {
+			log.Error().Err(err).Msg("failed to restore from aws s3")
 		}
 	}
+	blockchain.LoadDB()
+	sup.LoadStateDB(accountStorage)
+	blockchainSvc := &blockchain.Service{}
+
+	lastBlock := blockchainSvc.GetLastBlock()
+
+	go syncer.SyncAllAccounts(accountStorage)
+
+	var lbh cmn.HexBytes
+	lastBlockHash := lastBlock.GetHeader().GetBlock_ID().GetBlockHash()
+	lbh = lastBlockHash
+	lbHeight := lastBlock.GetHeader().GetHeight()
+
+	log.Info().Msgf("Last Block Hash: %v", lbh)
+	log.Info().Msgf("Height: %v", lbHeight)
+
+	s := lastBlock.GetHeader().GetTime().GetSeconds()
+	ts := time.Unix(s, 0)
+	log.Info().Msgf("Timestamp: %v", ts)
+
+	var stateRootHex cmn.HexBytes
+	stateRoot = lastBlock.GetHeader().GetStateRoot()
+	stateRootHex = stateRoot
+	log.Info().Msgf("State root: %v", stateRootHex)
+
+	supsvc.SetEnv(env)
+	supsvc.SetWaitTime(waitTime)
+	supsvc.SetNoOfPeersInGroup(noOfPeersInGroup)
+	supsvc.SetBackup(backup)
+
+	go func() {
+		for {
+			mu := supsvc.GetMutex()
+			mu.Lock()
+			// Check for deactivated validators and remove them from supervisor list
+			for _, v := range supsvc.Validator {
+				if !net.ConnectionStateExists(v.Address) {
+					supsvc.RemoveValidator(v.Address)
+				}
+			}
+			mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
 
 	for {
-		if *supervisorFlag {
-			supsvc.SetEnv(env)
-			supsvc.SetWaitTime(waitTime)
-			supsvc.SetNoOfPeersInGroup(noOfPeersInGroup)
-			supsvc.SetStateRoot(stateRoot)
-			supsvc.SetBackup(backup)
-			// Check for deactivated validators and remove them from supervisor list
-			if supsvc.Validator != nil && len(supsvc.Validator) > 0 {
-				for _, v := range supsvc.Validator {
-					if !net.ConnectionStateExists(v.Address) {
-						supsvc.RemoveValidator(v.Address)
-					}
-				}
-			}
-
-			lastBlock := blockchainSvc.GetLastBlock()
-			stateRoot = lastBlock.GetHeader().GetStateRoot()
-			supsvc.SetStateRoot(stateRoot)
-			baseBlock, err := supsvc.ProcessTxs(lastBlock, net)
-			if err != nil {
-				log.Error().Msg(err.Error())
-			}
-			if baseBlock != nil {
-				err = blockchainSvc.AddBaseBlock(baseBlock)
-				if err != nil {
-					log.Error().Msgf("Failed to Add Base Block: %v", err)
-				}
-
-				var bbh, pbbh cmn.HexBytes
-				pbbh = baseBlock.Header.LastBlockID.BlockHash
-				bbh = baseBlock.GetHeader().GetBlock_ID().GetBlockHash()
-				log.Info().Msg("New Block Added")
-				log.Info().Msgf("Block Id: %v", bbh.String())
-				log.Info().Msgf("Last Block Id: %v", pbbh.String())
-				log.Info().Msgf("Block Height: %v", baseBlock.GetHeader().GetHeight())
-
-				s := lastBlock.GetHeader().GetTime().GetSeconds()
-				ts := time.Unix(s, 0)
-				log.Info().Msgf("Timestamp : %v", ts)
-
-				var stateRoot cmn.HexBytes
-				stateRoot = baseBlock.GetHeader().GetStateRoot()
-				log.Info().Msgf("State root : %v", stateRoot)
-			}
+		supsvc.SetStateRoot(stateRoot)
+		lastBlock := blockchainSvc.GetLastBlock()
+		stateRoot = lastBlock.GetHeader().GetStateRoot()
+		supsvc.SetStateRoot(stateRoot)
+		baseBlock, err := supsvc.ProcessTxs(lastBlock, net)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to process txs")
+			continue
 		}
 
+		if err := blockchainSvc.AddBaseBlock(baseBlock); err != nil {
+			log.Error().Err(err).Msg("Failed to Add Base Block")
+			continue
+		}
+
+		var (
+			pbbh cmn.HexBytes = baseBlock.Header.LastBlockID.BlockHash
+			bbh  cmn.HexBytes = baseBlock.GetHeader().GetBlock_ID().GetBlockHash()
+		)
+
+		log.Info().Msg("New Block Added")
+		log.Info().Msgf("Block Id: %v", bbh.String())
+		log.Info().Msgf("Last Block Id: %v", pbbh.String())
+		log.Info().Msgf("Block Height: %v", baseBlock.GetHeader().GetHeight())
+
+		s := lastBlock.GetHeader().GetTime().GetSeconds()
+		ts := time.Unix(s, 0)
+		log.Info().Msgf("Timestamp : %v", ts)
+
+		var stateRoot cmn.HexBytes
+		stateRoot = baseBlock.GetHeader().GetStateRoot()
+		log.Info().Msgf("State root : %v", stateRoot)
 	}
 }
